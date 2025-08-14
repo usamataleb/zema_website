75fabc03a7197ebba1005bc4566b787caf535d97
usamataleb <mamenmasau@gmail.com>
2025-08-14 11:40:10 +0300

    Array Added to New Event Cards

--- src/components/NewsEvents.jsx
+++ src/components/NewsEvents.jsx
@@ -1,6 +1,34 @@
 import NewsCard from "./NewsCard";
 import Card from "./NewsCard";
 
+
+const NewsData = [
+  {
+    image: "img/about.jpg",
+    title: "News Title 1",
+    description: "Brief description of news 1.",
+    // date: "2023-10-01",
+  },
+  {
+    image: "img/about.jpg",
+    title: "News Title 2",
+    description: "Brief description of news 2.",
+    // date: "2023-10-02",
+  },
+  {
+    image: "img/about.jpg",
+    title: "News Title 3",
+    description: "Brief description of news 3.",
+    // date: "2023-10-03",
+  },
+  {
+    image: "img/about.jpg",
+    title: "News Title 4",
+    description: "Brief description of news 4.",
+    // date: "2023-10-04",
+  },
+];
+
 const NewsEvents = () => {
   return (
     <div className="container-xxl py-5">
@@ -9,41 +37,16 @@ const NewsEvents = () => {
           <h1 className="display-6 mb-5">News & Events</h1>
         </div>
         <div className="row g-4">
-          <div className="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-            <NewsCard
-              image="img/about.jpg"
-              title="News Title 1"
-              description="Brief description of news 1."
-              // date="2023-10-01"
-            />
-          </div>
-          <div className="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
-            <NewsCard
-              image="img/about.jpg"
-              title="News Title 2"
-              description="Brief description of news 2."
-              // date="2023-10-02"
-            />
-          </div>
-          <div className="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
-            <NewsCard
-              image="img/about.jpg"
-              title="News Title 3"
-              description="Brief description of news 3."
-              // date="2023-10-03"
-            />
-          </div>
-            <div
-              className="col-lg-3 col-md-6 wow fadeInUp"
-              data-wow-delay="0.7s"
-            >
+          {NewsData.map((news, index) => (
+            <div className="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay={`${index * 0.1}s`} key={index}>
               <NewsCard
-                image="img/about.jpg"
-                title="News Title 4"
-                description="Brief description of news 4."
-                // date="2023-10-04"
+                image={news.image}
+                title={news.title}
+                description={news.description}
+                // date={news.date}
               />
             </div>
+          ))}
           </div>
         </div>
       </div>
